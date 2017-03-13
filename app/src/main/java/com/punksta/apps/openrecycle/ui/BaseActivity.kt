26ddf073b3c9e79adba645a087e06ff11ff2d771
punksta <skinnystas@gmail.ocm>
2017-03-13 11:04:03 +0300

    added fabric

--- app/src/main/java/com/punksta/apps/openrecycle/ui/BaseActivity.kt
+++ app/src/main/java/com/punksta/apps/openrecycle/ui/BaseActivity.kt
@@ -2,15 +2,23 @@ package com.punksta.apps.openrecycle.ui
 
 import android.app.Activity
 import android.content.pm.PackageManager
+import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
+import com.crashlytics.android.Crashlytics
 import com.punksta.apps.openrecycle.R
+import io.fabric.sdk.android.Fabric
 
 /**
  * Created by stanislav on 3/11/17.
  */
 open class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
+    }
 
     override fun onStart() {
         super.onStart()
